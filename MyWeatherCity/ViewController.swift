7461711cef39f31852c621d4a2ef9ef6235d8199
sergeyvip84 <87651037+sergeyvip84@users.noreply.github.com>
2022-05-01 15:08:52 +0300

    Initial Commit

---
+++ MyWeatherCity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeatherCity
+//
+//  Created by Serhii Palamarchuk on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
